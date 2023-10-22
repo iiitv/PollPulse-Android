a407a36fd6f52a54559b33b6f4731be382c9633b
Kedar200 <Kedardeshmukh2003@gmail.com>
2023-10-23 04:13:31 +0530

    Merge Solved

--- app/src/main/java/com/example/votingapp/LoginPage.java
+++ app/src/main/java/com/example/votingapp/LoginPage.java
@@ -1,20 +1,34 @@
 package com.example.votingapp;
 
-import android.content.Intent;
-import android.view.View;
+import static com.example.votingapp.R.id.login;
+import static com.example.votingapp.R.id.signin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginPage extends AppCompatActivity{
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-    public View.OnClickListener click(){
+public class LoginPage extends AppCompatActivity{
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        getSupportActionBar().hide();
+        button =findViewById(login);
+        button.setOnClickListener(this::click);
+
+
+
+    }
+
+    public void click(View view) {
         Intent intent = new Intent(this, Verification_page.class);
 
         startActivity(intent);
-
-        return null;
     }
-
 
 
 }
